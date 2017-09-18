d307f21fc5b25cfcff7adeb385230d624297b291
Ethan Gundry <gundryethan@gmail.com>
2017-09-17 20:04:11 -0700

    Finished validation and controllers

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,10 +1,14 @@
+
 package org.launchcode.controllers;
 
+import org.launchcode.models.*;
+import org.launchcode.models.data.JobFieldData;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,7 +28,8 @@ public class JobController {
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
-
+        Job myJob = this.jobData.findById(id);
+        model.addAttribute("job", myJob);
         return "job-detail";
     }
 
@@ -41,7 +46,21 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
 
-        return "";
+        if(errors.hasErrors()){
+
+            return "new-job";
+        }
+
+        Employer employer= jobData.getEmployers().findById(jobForm.getEmployerId());
+        Location location= jobData.getLocations().findById(jobForm.getLocationId());
+        PositionType positionType=jobData.getPositionTypes().findById(jobForm.getPositionTypeId());
+        CoreCompetency coreCompetency=jobData.getCoreCompetencies().findById(jobForm.getSkillId());
+
+        Job job= new Job(jobForm.getName(),employer,location,positionType,coreCompetency);
+        jobData.add(job);
+        int id = job.getId();
+
+        return "redirect:/job?id="+id;
 
     }
 }
